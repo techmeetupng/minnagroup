d2ec148fafb05f2e1ba4132a6a46890c1ddb5c0e
Hassan Sani <saniyhassan@gmail.com>
2017-09-26 11:32:50 +0100

    Minna update

--- js/app.js
+++
@@ -1,8 +0,0 @@
-new Vue ({
-	el: '#app',
-	data: {
-		title: 'The #MinnaTechMeetUp',
-		slogan: 'This is not the page built during the coding session we are uploading it check back soon',
-		imgSrc: '/images/mine.jpg'
-	}
-})
